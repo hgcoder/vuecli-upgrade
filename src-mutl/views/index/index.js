51f2fc7ed154bb1d1e65f81550ee6e77a76bba70
unknown <16081267@sn.suning.ad>
2017-02-10 09:39:52 +0800

    mutl-code

---
+++ src-mutl/views/index/index.js
@@ -0,0 +1,33 @@
+
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-02-05 20:06:22
+ * @version $Id$
+ */
+import Vue from "vue"
+import $ from "jquery"
+
+new Vue({
+	el:"#app",
+	data: function(){
+        return { 
+        	visible: false 
+        }
+  	},
+    methods: {
+        handleOpen:function(){
+
+        },
+        handleClose:function(){
+        	
+        }
+    },
+    created(){
+    	console.log(3);
+    	
+    },
+    mounted(){
+
+    }
+})
